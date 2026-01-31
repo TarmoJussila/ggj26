789ccca5840a15df88f5119d94fc84d851286c71
Aleksi Toivonen <aleksi.toivonen@zaibatsu.fi>
2026-02-01 01:36:35 +0200

    hazard

--- Assets/Scripts/Gameplay/Hazard.cs
+++ Assets/Scripts/Gameplay/Hazard.cs
@@ -1,9 +1,22 @@
+using System;
 using UnityEngine;
 
 namespace Logbound.Gameplay
 {
     public class Hazard : MonoBehaviour
     {
+        [SerializeField] private float _lifetime;
+        private float _age;
         [field: SerializeField] public int DamagePerTick { get; private set; }
+
+        private void Update()
+        {
+            _age += Time.deltaTime;
+
+            if (_age > _lifetime && _lifetime > 0.1f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
