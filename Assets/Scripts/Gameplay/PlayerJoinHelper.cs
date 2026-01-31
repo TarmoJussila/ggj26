6ba744f04ed244f1615712b84f40bae2d7562a58
TuomoO <tuomo.oksanen@zaibatsu.fi>
2026-01-31 21:16:13 +0200

    How to join on screen help when there is no players

--- Assets/Scripts/Gameplay/PlayerJoinHelper.cs
+++ Assets/Scripts/Gameplay/PlayerJoinHelper.cs
@@ -5,11 +5,22 @@ namespace Logbound.Gameplay
 {
     public class PlayerJoinHelper : MonoBehaviour
     {
+        [SerializeField] private Camera howToJoinCamera;
+        [SerializeField] private GameObject howToJoinCanvas;
+
+        private int _playerJoinedCount;
+        
         public static event Action OnPlayerAdded;
         public static event Action OnPlayerRemoved;
-        
+
         private void OnPlayerJoined()
         {
+            _playerJoinedCount++;
+            if (_playerJoinedCount == 1)
+            {
+                howToJoinCanvas.SetActive(false);
+                howToJoinCanvas.SetActive(false);
+            }
             OnPlayerAdded?.Invoke();
         }
 
