b4dc02d8bf24a26f9261272f4b09887904f545bb
juhoturpeinen <104358560+juhoturpeinen@users.noreply.github.com>
2026-01-31 17:12:53 +0200

    Rat audio

---
+++ Assets/Scripts/Rats/RatAudio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Logbound.Rats
+{
+    public class RatAudio : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private AudioSource _audioSource;
+
+        private void Start()
+        {
+            _audioSource.Play();
+            _audioSource.time = Mathf.Lerp(0, 30f, Random.Range(0, 1));
+        }
+    }
+}
