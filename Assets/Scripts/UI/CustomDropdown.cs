e519d60d6134d61c20a68ebcbef7eb8e2ad564df
TuomoO <tuomo.oksanen@zaibatsu.fi>
2026-02-01 03:21:01 +0200

    Set hard coded height for resolution drop down menu

---
+++ Assets/Scripts/UI/CustomDropdown.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Logbound.UI
+{
+    public class CustomDropdown : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            var child = GameObject.Find("Dropdown List");
+            if (child == null)
+            {
+                Debug.LogWarning("Dropdown List not found");
+                return;
+            }
+
+            ((RectTransform)child.transform).sizeDelta = new Vector2(0, 550);
+        }
+    }
+}
