3c1fa231830546d9ec1ca6287568c1d8ff92f2f9
juhoturpeinen <104358560+juhoturpeinen@users.noreply.github.com>
2026-02-01 12:32:39 +0200

    Fireplace sound.

--- Assets/Scripts/Gameplay/Fireplace.cs
+++ Assets/Scripts/Gameplay/Fireplace.cs
@@ -8,6 +8,8 @@ namespace Logbound.Gameplay
         [SerializeField] private float _maxBurnTime;
         [SerializeField] private float _burnTimePerLog;
         [SerializeField] private float _playerHeatingEffect;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private float _audioVolume;
 
         public float BurnTimeLeft { get; private set; }
 
@@ -23,7 +25,7 @@ namespace Logbound.Gameplay
             BurnTimeLeft -= Time.deltaTime;
 
             BurnTimeLeft = Mathf.Clamp(BurnTimeLeft, 0, _maxBurnTime);
-            
+            _audioSource.volume = _audioVolume * (BurnTimeLeft / _maxBurnTime);
             foreach (var particle in _burnParticles)
             {
                 particle.transform.localScale = Vector3.one * BurnTimeLeft / _maxBurnTime;
